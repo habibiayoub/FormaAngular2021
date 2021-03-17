5a96b0dbef432c8518a9eae183c7b2b7af7d1e9c
Habibi Ayoub <habibiayoub87@gmail.com>
2021-03-17 16:33:41 +0100

    adding tree component and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FormaAngular2021';
+  title = 'HABIBI Ayoub';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { GrandChildComponent } from './grand-child/grand-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildAComponent,
+    ChildBComponent,
+    GrandChildComponent
   ],
   imports: [
     BrowserModule,
